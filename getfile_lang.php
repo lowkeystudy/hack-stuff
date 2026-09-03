0b2e42597aa15d041c90a9d537f7177c5fedba27
lowkeystudy <idkwhyiamusingthisshjt@gmail.com>
2026-09-03 21:04:27 +0700

    Update getfile_lang.php

--- getfile_lang.php
+++ getfile_lang.php
@@ -1,3 +1,11 @@
 <?php
-echo file_get_contents('index.php');
+echo "<pre>\n";
+// Dùng highlight_file hoặc show_source để hiển thị mã nguồn PHP mà không bị thực thi
+if (file_exists('index.php')) {
+    highlight_file('index.php');
+} else {
+    // Dự phòng đọc đường dẫn tuyệt đối
+    highlight_file('/challenge/web-serveur/ch13/index.php');
+}
+echo "\n</pre>";
 ?>
